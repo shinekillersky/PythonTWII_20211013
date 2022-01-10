535057fef06736c5094175f939710a96068b56d2
shine <shinekillersky@gmail.com>
2022-01-11 00:22:20 +0800

    python

--- 資料庫/ChartAndBWIBBU.py
+++ 資料庫/ChartAndBWIBBU.py
@@ -18,7 +18,7 @@ datas = cursor.fetchall()
 print(datas)
 # 繪圖前準備
 x_datas = []  # x 軸時間序列資料
-y_datas = []  # u 軸本益比序列資料
+y_datas = []  # y 軸本益比序列資料
 for data in datas:
     x_datas.append(data[0])
     y_datas.append(data[1])
